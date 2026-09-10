925643921fa4ac1010e4728d090f706ae0e6fb0f
João Antonio <contatojoao87@gmail.com>
2026-09-10 19:49:35 -0300

    feat: criar estilo mobile e desktop para o header

---
+++ modules/script.js
@@ -0,0 +1,6 @@
+import handleMenuHamburguer from "./menu-hamburguer.js";
+
+const btnHamburguer = document.querySelector(".menu-hamburguer");
+const menuDropdown = document.querySelector(".menu-dropdown");
+
+handleMenuHamburguer(btnHamburguer, menuDropdown);
